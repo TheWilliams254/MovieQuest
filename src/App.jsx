ce4d15a7d86746fa68302eeca7a7c5c5ca1f110b
william <williamwambugu663@gmail.com>
2025-04-19 16:07:15 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Home from "./pages/Home";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
